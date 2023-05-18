d37f040c41fe1d054d35b6df8d15ac9e80e74bd8
Kevin Chevreuil - Kaisen <kaisen@kaisenlinux.org>
2023-05-19 01:46:14 +0200

    Update the documentation and manpages for the 2.3 release

--- kaisen-manpages/commands/assets/packages/doc-versions.js
+++ kaisen-manpages/commands/assets/packages/doc-versions.js
@@ -1,6 +1,10 @@
 var versionNodes = [
   {
   version: "Sort by categories", 
-  url: "file:///usr/share/kaisen-manpages/readme.html"
-  }
+  url: "https://kaisenlinux.org/manpages/readme.html"
+  },
+  {
+    version: "Sort by configuration files", 
+    url: "https://kaisenlinux.org/manpages/configuration_files/readme.html"
+    }
 ]
